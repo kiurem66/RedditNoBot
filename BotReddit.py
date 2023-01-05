88737dd61f14e677d3391128752b08f2a11c2da3
Matteo <orlandomatteo38@gmail.com>
2023-01-05 20:19:45 +0100

    aggiunta modulo twitch e mini fix leggibilità

--- BotReddit.py
+++ BotReddit.py
@@ -8,25 +8,25 @@ def main():
     f.close()
     fernet=Fernet(key)
     with open('credentials.txt', 'rb') as f:
-        CLIENT_ID=fernet.decrypt(f.readline()).decode()
+        CLIENT_ID_REDDIT=fernet.decrypt(f.readline()).decode()
         API_KEY=fernet.decrypt(f.readline()).decode()
         pw=fernet.decrypt(f.readline()).decode()
     f.close()
-    FLAIR_LIVE = '4f77c9a4-ef7a-11eb-8ee5-aefe156a2d70'
-    SABREDDIT = 'SabakuNoMaiku'
+    FLAIR = '4f77c9a4-ef7a-11eb-8ee5-aefe156a2d70'
+    SUBREDDIT = 'SabakuNoMaiku'
     HOURS_THRESHOLD = 6.0
     MIN_COM = 5
     un = 'CostruttoViola'
-    ua = 'script:RedditNoBot:v1.2.0 (by /u/CostruttoViola)'
+    ua = 'script:RedditNoBot:v1.2.5 (by /u/CostruttoViola)'
 
     reddit = praw.Reddit(
-        client_id=CLIENT_ID,
+        client_id=CLIENT_ID_REDDIT,
         client_secret=API_KEY,
         username=un,
         password=pw,
         user_agent=ua
     )
-    delete_specific_post(reddit,SABREDDIT,FLAIR_LIVE,HOURS_THRESHOLD,MIN_COM)
+    delete_specific_post(reddit,SUBREDDIT,FLAIR,HOURS_THRESHOLD,MIN_COM)
 
 
 if __name__ == '__main__':
